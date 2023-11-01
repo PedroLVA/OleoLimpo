e7336abbe03535bff1f2a69a5a73465740910408
FutureMinds2023 <144184197+FutureMinds2023@users.noreply.github.com>
2023-11-01 19:30:18 -0300

    Update and rename conexao_bd.js to conexao.php

---
+++ conexao.php
@@ -0,0 +1,8 @@
+<?php
+
+ $servidor = 'fdb1034.awardspace.net';
+ $db_nome = '4045440_oleodobem';
+ $db_senha = 'FutureMinds2023??';
+ $db_usuario = '4045440_oleodobem';
+ 
+ $conexao = new mysqli($servidor,$db_nome,$db_senha,$db_usuario);?>
