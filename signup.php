88e48c5c26d81c739de1e76c8a2b35e6aef5636d
Pedro Athayde <pedro_athayde@outlook.com>
2023-12-12 16:03:08 -0300

    feat: better organization of folders

--- signup.php
+++ signup.php
@@ -13,16 +13,14 @@
                             <div class="mb-3"><input class="shadow-sm form-control" type="text" name="sobrenome" placeholder="Sobrenome"></div>
                             <div class="idade mb-3"><input class="shadow-sm form-control" type="text" name="idade" placeholder="idade"></div>
                             <div class="email-input mb-3"><input class=" shadow-sm form-control" type="email" name="email" placeholder="E-mail"></div>
-                            <div class="cep mb-3"><input class=" shadow-sm form-control" type="text" name="cep" placeholder="CEP"></div>
+                            <div class="cep mb-3"><input class="shadow-sm form-control" type="text" name="cep" placeholder="CEP"></div>
                             <div class="rua mb-3"><input class=" shadow-sm form-control" type="text" name="rua" placeholder="Rua"></div>
                             <div class="numero-rua mb-3"><input class=" shadow-sm form-control" type="text" name="numero" placeholder="Numero rua"></div>
                             <div class="mb-3"><input class="shadow-sm form-control" type="password" name="password" placeholder="Senha"></div>
                             <div class="mb-3"><input class="shadow-sm form-control" type="password" name="password_repeat" placeholder="Repetir Senha"></div>   
                         </div>
                                                 
-                        <div class="mb-5">
-                            <button class="btn btn-primary shadow" type="submit" style="background: rgb(1,113,61);">Criar Conta</button>
-                        </div>
+                        
                     </form>
                     <p class="text-muted">Tem uma conta?&nbsp;<a href="login.html">Entrar<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icon-tabler-arrow-narrow-right">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
@@ -31,6 +29,7 @@
                                 <line x1="15" y1="8" x2="19" y2="12"></line>
                             </svg></a>&nbsp;</p>
                 </div>
+
             </div>
         </div>
     </section>
